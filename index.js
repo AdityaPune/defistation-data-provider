3f6bdac55d44154d4e624a2f0276e0e37b421416
Aditya <adityakho999@gmail.com>
2021-10-27 13:10:13 +0530

    First commit

--- index.js
+++ index.js
@@ -1,126 +1,150 @@
-"use strict"
+"use strict";
 const ethers = require("ethers");
-const BStablePool = require('./abi/BStablePool.json');
-const BEP20 = require('./abi/BEP20.json');
-const BigNumber = require('bignumber.js');
-const https = require('https');
-const log4js = require('log4js');
-const config = require('./conf/conf.js');
+const abi = require("./abi/ABI.json");
+const BigNumber = require("bignumber.js");
+const https = require("https");
+const log4js = require("log4js");
+const config = require("./conf/conf.js");
+const sdk = require("@defillama/sdk");
+const moment = require("moment");
 
 log4js.configure(config.log4jsConfig);
-const logger = log4js.getLogger('Defistation\'s Data Provider');
-logger.info('Defistation\'s Data Provider start.');
+const logger = log4js.getLogger("Defistation's Data Provider");
+logger.info("Defistation's Data Provider start.");
 
-const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/');
+async function gettvl() {
+  const DAOvault = {
+    Elon: "0x2D9a136cF87D599628BCBDfB6C4fe75Acd2A0aA8",
+    Cuban: "0x2AD9F8d4c24652Ea9F8A954F7E1FdB50a3bE1DFD",
+    Citadel: "0x8fE826cC1225B03Aa06477Ad5AF745aEd5FE7066",
+    FAANG: "0x9ee54014e1E6CF10fD7E9290FdB6101fd0d5D416",
+    Metaverse: "0x5b3ae8b672a753906b1592d44741f71fbd05ba8c",
+    CitadelV2: "0x3845d7c09374df1ae6ce4728c99dd20d3d75f414",
+    //MoneyPrinter: "0x3DB93e95c9881BC7D9f2C845ce12e97130Ebf5f2",
+  };
+  const timestamp = moment().unix();
+  let block = await sdk.api.util.lookupBlock(timestamp);
 
-const pool1Address = '0x9c00954A8A58f5DDa8C011D6233093763F13C8Da';
+  let [ElonTVL, CubanTVL, CitadelTVL, FAANGTVL, MetaverseTVL, CitadelV2TVL] =
+    await Promise.all([
+      sdk.api.abi.call({
+        target: DAOvault.Elon, // contract address
+        abi: abi.getAllPoolInUSD, // erc20:methodName
+        block: block[block], // Current block number
+      }),
 
-const pool2Address = '0x27F545300F7b93c1c0184979762622Db043b0805';
+      sdk.api.abi.call({
+        target: DAOvault.Cuban, // contract address
+        abi: abi.getAllPoolInUSD, // erc20:methodName
+        block: block[block], // Current block number
+      }),
+      sdk.api.abi.call({
+        target: DAOvault.Citadel, // contract address
+        abi: abi.getAllPoolInUSD, // erc20:methodName
+        block: block[block], // Current block number
+      }),
 
-const daiAddress = '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3';
+      sdk.api.abi.call({
+        target: DAOvault.FAANG, // contract address
+        abi: abi.getTotalValueInPool, // erc20:methodName
+        block: block[block], // Current block number
+      }),
 
-const busdAddress = '0xe9e7cea3dedca5984780bafc599bd69add087d56';
+      sdk.api.abi.call({
+        target: DAOvault.Metaverse, // contract address
+        abi: abi.getAllPoolInUSD, // erc20:methodName
+        block: block[block], // Current block number
+      }),
+      sdk.api.abi.call({
+        target: DAOvault.CitadelV2, // contract address
+        abi: abi.getAllPoolInUSD, // erc20:methodName
+        block: block[block], // Current block number
+      }),
+    ]);
+  ElonTVL = parseInt(ElonTVL.output) / 10 ** 6;
+  CitadelTVL = parseInt(CitadelTVL.output) / 10 ** 6;
+  CubanTVL = parseInt(CubanTVL.output) / 10 ** 6;
+  MetaverseTVL = parseInt(MetaverseTVL.output) / 10 ** 18;
+  FAANGTVL = parseInt(FAANGTVL.output) / 10 ** 18;
+  CitadelV2TVL = parseInt(CitadelV2TVL.output) / 10 ** 18;
 
-const usdtAddress = '0x55d398326f99059ff775485246999027b3197955';
+  let tvl =
+    ElonTVL + CitadelTVL + CubanTVL + MetaverseTVL + FAANGTVL + CitadelV2TVL;
+  tvl = tvl.toFixed(2);
+  console.log(tvl);
 
-const qusdAddress = '0xb8c540d00dd0bf76ea12e4b4b95efc90804f924e';
-
-const pool1Contract = new ethers.Contract(pool1Address, BStablePool.abi, provider);
-const pool2Contract = new ethers.Contract(pool2Address, BStablePool.abi, provider);
-const daiContract = new ethers.Contract(daiAddress, BEP20.abi, provider);
-const busdContract = new ethers.Contract(busdAddress, BEP20.abi, provider);
-const usdtContract = new ethers.Contract(usdtAddress, BEP20.abi, provider);
-const qusdContract = new ethers.Contract(qusdAddress, BEP20.abi, provider);
-
-let arrP = new Array();
-
-arrP.push(daiContract.balanceOf(pool1Address));
-arrP.push(busdContract.balanceOf(pool1Address));
-arrP.push(usdtContract.balanceOf(pool1Address));
-arrP.push(qusdContract.balanceOf(pool2Address));
-arrP.push(busdContract.balanceOf(pool2Address));
-arrP.push(usdtContract.balanceOf(pool2Address));
-
-Promise.all(arrP).then(res => {
-    let p1DAI_Bal = new BigNumber(ethers.utils.formatEther(res[0]));
-    let p1BUSD_Bal = new BigNumber(ethers.utils.formatEther(res[1]));
-    let p1USDT_Bal = new BigNumber(ethers.utils.formatEther(res[2]));
-    let p2QUSD_Bal = new BigNumber(ethers.utils.formatEther(res[3]));
-    let p2BUSD_Bal = new BigNumber(ethers.utils.formatEther(res[4]));
-    let p2USDT_Bal = new BigNumber(ethers.utils.formatEther(res[5]));
-
-    let tvl = p1DAI_Bal.plus(p1BUSD_Bal).plus(p1USDT_Bal).plus(p2QUSD_Bal).plus(p2BUSD_Bal).plus(p2USDT_Bal);
-    logger.info('Total value locked: ' + tvl.toFormat(18, BigNumber.ROUND_DOWN));
-
-    let body = {
-        "tvl": Number(tvl.toFixed(4, BigNumber.ROUND_HALF_UP)),
-        "volume": 0,
-        "bnb": 0,
-        "data": {
-            "pairEntities": [
-                {
-                    "id": "0x9c00954A8A58f5DDa8C011D6233093763F13C8Da",
-                    "token0": {
-                        "symbol": "DAI"
-                    },
-                    "token1": {
-                        "symbol": "BUSD"
-                    },
-                    "token2": {
-                        "symbol": "USDT"
-                    }
-                },
-                {
-                    "id": "0x27F545300F7b93c1c0184979762622Db043b0805",
-                    "token0": {
-                        "symbol": "QUSD"
-                    },
-                    "token1": {
-                        "symbol": "BUSD"
-                    },
-                    "token2": {
-                        "symbol": "USDT"
-                    }
-                }
-            ]
+  let body = {
+    tvl: tvl,
+    volume: 0,
+    bnb: 0,
+    data: {
+      vaults: [
+        {
+          id: "0x2D9a136cF87D599628BCBDfB6C4fe75Acd2A0aA8",
+          name: "Elon",
         },
-        "test": false
-    };
-    let clientId = config.default.clientId;
-    let clientSecret = config.default.key;
-    let auth = 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64');
-    let bodyStr = JSON.stringify(body);
-    logger.info(bodyStr);
-    let headers = {
-        // 'Host': 'www.example.com',
-        'Authorization': auth,
-        'Content-Type': 'application/json',
-        'Content-Length': bodyStr.length
-    };
-    let options = {
-        host: 'api.defistation.io',
-        port: 443,
-        path: '/dataProvider/tvl',
-        method: 'POST',
-        headers: headers
-    };
-    let req = https.request(options, (res) => {
-        logger.info(`STATUS: ${res.statusCode}`);
-        logger.info(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            logger.info(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-            logger.info('No more data in response.');
-        });
+        {
+          id: "0x2AD9F8d4c24652Ea9F8A954F7E1FdB50a3bE1DFD",
+          name: "Cuban",
+        },
+        {
+          id: "0x8fE826cC1225B03Aa06477Ad5AF745aEd5FE7066",
+          name: "Citadel",
+        },
+        {
+          id: "0x9ee54014e1E6CF10fD7E9290FdB6101fd0d5D416",
+          name: "FAANG",
+        },
+        {
+          id: "0x5b3ae8b672a753906b1592d44741f71fbd05ba8c",
+          name: "Metaverse",
+        },
+        {
+          id: "0x3845d7c09374df1ae6ce4728c99dd20d3d75f414",
+          name: "CitadelV2",
+        },
+      ],
+    },
+    test: false,
+  };
+  let clientId = config.default.clientId;
+  let clientSecret = config.default.key;
+  let auth =
+    "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64");
+  let bodyStr = JSON.stringify(body);
+  logger.info(bodyStr);
+  let headers = {
+    // 'Host': 'www.example.com',
+    Authorization: auth,
+    "Content-Type": "application/json",
+    "Content-Length": bodyStr.length,
+  };
+  let options = {
+    host: "api.defistation.io",
+    port: 443,
+    path: "/dataProvider/tvl",
+    method: "POST",
+    headers: headers,
+  };
+  let req = https.request(options, (res) => {
+    logger.info(`STATUS: ${res.statusCode}`);
+    logger.info(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding("utf8");
+    res.on("data", (chunk) => {
+      logger.info(`BODY: ${chunk}`);
     });
-
-    req.on('error', (e) => {
-        logger.error(`problem with request: ${e.message}`);
+    res.on("end", () => {
+      logger.info("No more data in response.");
     });
+  });
 
-    // write data to request body
-    req.write(bodyStr);
-    req.end();
-});
+  req.on("error", (e) => {
+    logger.error(`problem with request: ${e.message}`);
+  });
+
+  // write data to request body
+  req.write(bodyStr);
+  req.end();
+}
+
+gettvl();
